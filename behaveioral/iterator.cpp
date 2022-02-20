90f3ff70348967a2f31f56cd8faab727ef549669
noamTzuber <76451297+noamTzuber@users.noreply.github.com>
2022-02-20 11:59:49 +0200

    Update iterator.cpp

--- behaveioral/iterator.cpp
+++ behaveioral/iterator.cpp
@@ -2,37 +2,16 @@
 
 using namespace std;
 
-class dataStructure;
 class doubleIterator;
 
-class Iterator {
-protected:
-    dataStructure *structure;
+class structure {
 public:
-    Iterator(dataStructure *structure) : structure(structure) {}
-
-    virtual int *begin() {};
-
-    virtual int *end() {};
-
-    virtual int *next() {};
-
-    virtual int *currentItem() {};
-
-
-};
-
-class dataStructure {
-protected:
     int *arr1;
     int *arr2;
     int size1;
     int size2;
-public:
-    dataStructure(int *arr1, int *arr2, int size1, int size2) : arr1(arr1), arr2(arr2), size1(size1), size2(size2) {}
-    doubleIterator *createIterator() {
-//        return new doubleIterator(this);
-    };
+
+    structure(int *arr1, int *arr2, int size1, int size2) : arr1(arr1), arr2(arr2), size1(size1), size2(size2) {};
 
     int *getArr1() {
         return arr1;
@@ -51,6 +30,24 @@ public:
     }
 };
 
+class Iterator {
+protected:
+    structure *structure;
+public:
+    Iterator(class structure *structure1) : structure(structure1) {};
+
+    virtual int *begin()=0;
+
+    virtual int *end() =0;
+
+    virtual int *next()=0;
+
+    virtual int *currentItem()=0;
+
+
+};
+
+
 class doubleIterator : public Iterator {
     int *arr1;
     int *arr2;
@@ -59,8 +56,9 @@ class doubleIterator : public Iterator {
     int *current;
     int *endValue;
     int *beginValue;
+
 public:
-    doubleIterator(dataStructure *structure) : Iterator(structure) {
+    doubleIterator(class structure *structure1) : Iterator(structure1) {
         this->arr1 = structure->getArr1();
         this->arr2 = structure->getArr2();
         this->size1 = structure->getSize1();
@@ -73,36 +71,66 @@ public:
             this->endValue = arr2 + this->size2 - 1;
         }
     }
-        int * begin() {
-            return this->beginValue;
-        }
 
-        int *end() {
-            return this->endValue;
-        }
-        int *currentItem() {
-            return this->current;
-        }
+    int *begin() {
+        return this->beginValue;
+    }
 
-        int *next() {
-            if (current == endValue) {
-                cout << "end of arrays" << endl;
-            } else if (current >= arr1 && current < arr1 + size1) {
-                int placeInArr2 = (current - arr1) / sizeof(int );
-                current = arr2 + placeInArr2;
-            } else {
-                int placeInArr2 = (current - arr2) / sizeof(int );
-                current = arr2 + placeInArr2 + 1;
-            }
-            return current;
-        };
+    int *end() {
+        return this->endValue;
+    }
 
+    int *currentItem() {
+        return this->current;
+    }
+
+    int *next() {
+        // if we in the end of the array
+        if (current == endValue) {
+            cout << "end of arrays" << endl;
+            // if it is the first value
+        } else if (current == beginValue) {
+            int long long placeInArr = (current - arr1) ;
+            current = arr2 + placeInArr;
+            // if we are in first array move to the second - at the same place
+        } else if (current >=arr1 && current <= arr1+ size1) {
+            int long long placeInArr = (current - arr1)  ;
+            current = arr2 + placeInArr ;
+            // if we are in the second array move the first and move on one variable
+        } else {
+            int long long placeInArr = (current - arr2) ;
+            current = arr1 + placeInArr + 1;
+        }
+        return
+                current;
     };
 
-//int main(){
-//    int arr1[]={1,2,3};
-//    int arr2[]={4,5,6};
-//    Iterator it=*(new dataStructure(arr1,arr2,3,3))->createIterator();
-//    cout<<*(it.begin())<<endl;
-//    return 0;
-//}
+};
+
+class dataStructure : public structure {
+protected:
+    int *arr1;
+    int *arr2;
+    int size1;
+    int size2;
+public:
+    dataStructure(int *arr1, int *arr2, int size1, int size2) : structure(arr1, arr2, size1, size2), arr1(arr1),
+                                                                arr2(arr2), size1(size1), size2(size2) {}
+
+    doubleIterator *createIterator() {
+        return new doubleIterator(this);
+    };
+};
+
+
+int main() {
+
+    int arr1[] = {1, 2, 3};
+    int arr2[] = {4, 5, 6};
+    doubleIterator it = *(new dataStructure(arr1, arr2, 3, 3))->createIterator();
+    for (int i; i < 6; i++) {
+        cout << *(it.currentItem()) << endl;
+        it.next();
+    }
+    return 0;
+}
